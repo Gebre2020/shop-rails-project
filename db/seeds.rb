a3effbf84a5ee9faf93728a0998fe75cadb3a0de
Gebremeskel <gebre20122@gmail.com>
2021-04-24 10:11:00 -0500

    create attributes to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.create([
+#     {username: "test3", email: "test2@test3.com", password: "test3"},
+#     {username: "test4", email: "test2@test4.com", password: "test4"},
+#     {username: "test5", email: "test2@test5.com", password: "test5"}
+# ])
+
+# faker gem
+
+# 10.times do
+#     User.create(username: Faker::Name)
+# end
