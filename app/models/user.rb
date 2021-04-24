949ffec5c420830c03fc3c1b446eb5cd53637531
Gebremeskel <gebre20122@gmail.com>
2021-04-23 22:31:59 -0500

    association and validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   has_many :products
   has_many :order_items, through: :products
+
+  validates :username, :email, uniqueness: true, presence: true
 end
