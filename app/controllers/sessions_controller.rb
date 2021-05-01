af0f45ec4b68ad9b3c7ecf222a8fe5906afc185c
Gebremeskel <gebre20122@gmail.com>
2021-05-01 14:17:12 -0500

    adding sessions path code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,30 @@
 class SessionsController < ApplicationController
-    def welcome
+    
+    # responsible for creating a new session/logging in user
+    def home
+    end
+       
+    def new
+        @user = User.new
+    end
+    
+    def create
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            # logging in
+            session[:user_id] = user.id
+            redirect_to user_path(user), notice: "Logged in successfully"  # show page
+        else
+            flash[:message] = "Incorrect login info, please try again"
+            redirect_to "/login"
+        end
+    end
+    
+    def destroy
+        if !session[:name].nil?
+            session.delete :name
+          else
+          redirect_to '/'
+          end        
     end
 end
