f04f61769fef3fddd4d6a049e98458646f168918
Gebremeskel <gebre20122@gmail.com>
2021-05-02 09:45:14 -0500

    update redirect path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
- 
+
     def new
         @user = User.new
     end
@@ -8,13 +8,14 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
-            redirect_to user_path(@user)
+            redirect_to @user
         else
             render :new
         end
     end
 
     def show
+        redirect_if_not_logged_in
         @user = User.find_by_id(params[:id])
         redirect_to '/' if !@user
     end
