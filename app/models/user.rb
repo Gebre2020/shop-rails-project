2f0360df0914ae64f6a2491b36e2a1dc5355cb0e
Gebremeskel <gebre20122@gmail.com>
2021-05-02 09:52:00 -0500

    add  message

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,21 @@ class User < ApplicationRecord
   has_many :products  # array
   has_many :product_orders, through: :products
   
-  validates :username, :email, uniqueness: true, presence: true
+  # where does validates come from? activerecord
+  validates :username, presence: true
+  validates :email, uniqueness: true, presence: true
   validates :password, presence: true
   validates :password, length: { in: 6..10 }
+
+  # custom method
+  # Validate :is_a_valid_email?
+
+  # def is_a_valid_email?
+  #   if !email.match(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
+  #     errors.add(:email, "Give me a real email")
+  #   end
+  # end
+
+  # validator going to check that theres 2 words when full_name submitted
 
 end
